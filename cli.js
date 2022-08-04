9a21008bcf441349913af56ef1c5616a97987a89
Gabriel Galego <biel.galego2019@gmail.com>
2022-08-04 10:20:02 -0300

    terminando de validar links

--- cli.js
+++ cli.js
@@ -1,6 +1,17 @@
+import chalk from 'chalk';
 import pegaArquivo from '../lib-markdown/index.js';
+import validaUrls from './http-validacao.js';
 
 const caminho = process.argv;
-console.log(caminho);
 
-console.log(pegaArquivo(caminho[2]));
+async function processaTexto(caminhoDeArquivo) {
+  const resultado = await pegaArquivo(caminhoDeArquivo[2]);
+  if (caminho[3] === 'validar') {
+    console.log(chalk.yellow('links validos: '), await validaUrls(resultado));
+  } else {
+    console.log(chalk.yellow('lista de links'), resultado);
+  }
+
+}
+
+processaTexto(caminho);
---
+++ http-validacao.js
@@ -0,0 +1,39 @@
+import fetch from 'node-fetch';
+
+function manejaErros(erro) {
+  throw new Error(erro.message);
+}
+
+async function checaStatus(arrayURLs) {
+  try {
+    const arrayStatus = await Promise
+      .all(arrayURLs
+        .map(async url => {
+          const res = await fetch(url)
+          return `${res.status} - ${res.statusText}`;
+        })
+      )
+    return arrayStatus;
+  } catch (erro) {
+    manejaErros(erro);
+  }
+
+}
+
+function geraArrayDeLinks(arrayLinks) {
+  return arrayLinks.map(objetoLink => Object.values(objetoLink).join());
+}
+
+async function validaUrls(arrayLinks) {
+  const links = geraArrayDeLinks(arrayLinks);
+  const statusLinks = await checaStatus(links);
+
+  const resultados = arrayLinks.map((objeto, indice) => ({
+    ...objeto,
+    status: statusLinks[indice]
+  }))
+
+  return resultados;
+}
+
+export default validaUrls;
